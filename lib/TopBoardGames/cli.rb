70274c33c51b3502bcb8403224d544c6365e7e10
johanesteves <jsesteves91@gmail.com>
2017-09-20 19:10:48 -0400

    Added basic CLI UI

--- lib/TopBoardGames/cli.rb
+++ lib/TopBoardGames/cli.rb
@@ -1,6 +1,30 @@
-class TopBoardGames
+class TopBoardGames::CLI
 
   def call
-    puts "Top Board Games: "
+    menu
+    #list_boardgames
+  end
+
+
+  def menu
+    puts "Greetings! Here you will be able to see a list of the top 100 boardgames"
+    puts "Please choose with list you would like to see:",""
+    puts "1-20 --- 21-40 --- 41-60 --- 61-80 --- 81-100"
+
+    input  = gets.strip
+
+    if input == "1"
+      puts "", "Here are the following 1-20:"
+    elsif input == "2"
+      puts "","Here are the following 21-40:"
+    elsif input == "3"
+      puts "","Here are the following 41-0:"
+    elsif input == "4"
+      puts "","Here are the following 81-100:"
+    elsif input == "5"
+    else
+      puts "","You have entered an invalid input"
+    end
+
   end
 end
