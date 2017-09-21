3b21e466b9b82ba254c73d6fae417e7d9668c876
johanesteves <jsesteves91@gmail.com>
2017-09-21 15:49:20 -0400

    Add menu feature to CLI

--- lib/TopBoardGames/cli.rb
+++ lib/TopBoardGames/cli.rb
@@ -2,24 +2,34 @@ class TopBoardGames::CLI
 
   def call
     TopBoardGames::Scraper.new.make_boardgames
-    menu
+    start
   end
 
   def list_boardgames(input)
     @boardgames = TopBoardGames::Game.all
-
     @boardgames[input].each do |game|
-      puts "#{game.rank}. #{game.title} //Avg. Rating: #{game.avg_rating}"
+      puts "#{game.rank}.".ljust(5)+"#{game.title}"
     end
   end
 
   def navigation_menu
-    puts "","Please choose with list you would like to see:"
-    puts "(1)        (2)      (3)       (4)       (5)"
-    puts "1-20 --- 21-40 --- 41-60 --- 61-80 --- 81-100"
+    puts "","Please choose which list you would like to see:"
+    puts " (1)       (2)       (3)       (4)       (5)"
+    puts "01-20 --- 21-40 --- 41-60 --- 61-80 --- 81-100"
+    input  = gets.strip.downcase
+    inputs = {1 => [1,20], 2 => [21, 40], 3 => [41, 60], 4 => [61, 80], 5 => [81,100]}
+
+    if input.to_i > 0
+      puts "Here are the #{inputs[input.to_i][0]}-#{inputs[input.to_i][1]} top boardgames:", ""
+      list_boardgames((inputs[input.to_i][0])-1..(inputs[input.to_i][1])-1) # list_boardgames(0..19)
+      puts "========================================================="
+      puts "", "Enter boardgame number you wish to view", "Enter '[m]enu' to return the main menu", "Enter '[e]xit' to quit program" ,"" ,"Enter your selection:"
+    end
+
+
   end
 
-  def menu
+  def start
     puts "", "Greetings! Here you will be able to see a list of the top 100 boardgames"
     navigation_menu
     game_list
@@ -28,58 +38,35 @@ class TopBoardGames::CLI
   def game_list
     input = nil
     while input != "exit"
-      input  = gets.strip.downcase
-
-      if input == "1"
-        puts "Here are the following 1-20:", ""
-        list_boardgames(0..19)
-        puts "", "Choose which game you would like more information on or type '[m]enu'"
-        input = gets.strip.downcase
-        if input == "menu"
-          navigation_menu
-        else
-          display_game_description(@boardgames[input.to_i-1])
-        end
-
-      elsif input == "2"
-        puts "Here are the following 21-40:", ""
-        list_boardgames(20..39)
+      input = gets.strip.downcase
+      if ["menu", "m"].include?(input) #
         navigation_menu
-      elsif input == "3"
-        puts "Here are the following 41-60:", ""
-        list_boardgames(40..59)
-        navigation_menu
-      elsif input == "4"
-        puts "Here are the following 61-80:", ""
-        list_boardgames(60..79)
-        navigation_menu
-      elsif input == "5"
-        puts "Here are the following 81-100:", ""
-        list_boardgames(80..99)
-        navigation_menu
-      elsif input == "exit"
-        puts "Goodbye!!"
+      elsif input.to_i >0
+        display_game_description(@boardgames[input.to_i-1])
+      elsif ["exit", "e"].include?(input)
+        puts "", "Goodbye!!"
       else
         puts "You have entered an invalid input"
       end
     end
   end
 
-
   def display_game_description(game)
     puts "", "More information about:", "========================================================="
     puts game.title.upcase
     puts "========================================================="
-    puts "Overall Rank: #{game.rank}"
-    puts "Average Rating: #{game.avg_rating}"
-    puts "Weight: #{game.weight}"
+    puts "Overall Rank:       #{game.rank}"
+    puts "Average Rating:     #{game.avg_rating}"
+    puts "Weight (Out of 5):  #{game.weight.to_f.round(2)}"
     if game.min_playtime == game.max_playtime
       puts "Estimated Playtime: #{game.min_playtime} mins"
     else
       puts "Estimated Playtime: #{game.min_playtime} - #{game.max_playtime} mins"
     end
-    puts "Recommended Age: #{game.min_age}+"
-    puts "Description:"
+    puts "Recommended Age:    #{game.min_age}+"
+    puts "", "Description:"
     puts game.description, ""
+    puts "========================================================="
+    puts "", "Enter boardgame number you wish to view", "Enter '[m]enu' to return the main menu", "Enter '[e]xit' to quit program" ,"" ,"Enter your selection:"
   end
 end
